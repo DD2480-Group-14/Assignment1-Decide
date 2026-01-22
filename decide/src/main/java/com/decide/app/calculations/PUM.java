d91a3b8cadba38adc813fbf25b134740bc4be2f9
Maria Pettersson <mepet@kth.se>
2026-01-22 14:00:51 +0100

    Create code skeleton for PUM calculations
    Closes #26

---
+++ decide/src/main/java/com/decide/app/calculations/PUM.java
@@ -0,0 +1,16 @@
+package com.decide.app.calculations;
+
+public class PUM {
+
+  /* 
+  Function that compares the CMV with the LCM to generate PUM. 
+  */
+
+  public boolean[][] generatePUM() {
+    boolean[][] pum = new boolean[15][15];
+
+    // TO DO
+
+    return pum;
+  }
+}
