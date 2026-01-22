5c3928b218cc1d7b7a6f4e60304e3889c43a34c6
Melker Trané <melkert@kth.se>
2026-01-22 19:00:08 +0100

    added Point.java
    Also changed CMV.java to use this data structure instead

---
+++ decide/src/main/java/com/decide/app/model/Point.java
@@ -0,0 +1,21 @@
+
+/**
+ * Custom datastructure that contain the coordiate information of a planar point.
+ */
+
+public class Point {
+    
+
+    double x;
+    double y;
+
+    /**
+     * Creates a point with the given x and y coordinates.
+     * @param x x-position
+     * @param y y-position
+     */
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
