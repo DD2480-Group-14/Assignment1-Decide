eb0979dc024cb947dea5618ad8345f6581efb2d3
yoogway <aspdfiasdofijasdfoijasdfoij@psadijfaosidfjasdfasdf.com>
2026-01-22 13:18:09 +0100

    Create initial test classes for each computation

---
+++ decide/src/test/java/com/decide/app/computations/LaunchTest.java
@@ -0,0 +1,19 @@
+package com.decide.app;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for Launch computation.
+ */
+public class LaunchTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
---
+++ decide/src/test/java/com/decide/app/computations/PUMTest.java
@@ -0,0 +1,19 @@
+package com.decide.app;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for PUM computation.
+ */
+public class PUMTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
