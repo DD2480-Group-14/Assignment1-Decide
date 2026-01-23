9390b381121a113db04a1cdf41eb398a244d6364
Shrek <edwin03@live.se>
2026-01-23 15:17:09 +0100

    Add FUV.java and skeleton code for calculating the FUV

---
+++ decide/src/main/java/com/decide/app/calculations/FUV.java
@@ -0,0 +1,12 @@
+package com.decide.app.calculations;
+
+public class FUV {
+
+    // Generate the Final Unlocking Vector (FUV)
+    // from the PUV and PUM
+    Boolean[] calculate_FUV() {
+        Boolean[] b = new Boolean[10];
+        // todo
+        return b;
+    }
+}
