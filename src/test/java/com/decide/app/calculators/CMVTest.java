db124b6bf705e9416dd82bfcfe72920860f61624
yoogway <aspdfiasdofijasdfoijasdfoij@psadijfaosidfjasdfasdf.com>
2026-01-24 20:49:18 +0100

    Fix breaking changes in CMV tests that occured due to the new Parameters

--- src/test/java/com/decide/app/calculators/CMVTest.java
+++ src/test/java/com/decide/app/calculators/CMVTest.java
@@ -11,6 +11,7 @@ import com.decide.app.model.*;
  * Unit test for CMV computation.
  */
 public class CMVTest {
+    private Parameters randomParameters = new Parameters(1.0, 2.0, 3.0, 4.0, 5, 6, 7.0, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17.0, 18.0, 19.0);
 
     /**
      * Rigorous Test :-)
@@ -24,8 +25,7 @@ public class CMVTest {
     void lic5Positive() {
         int numpoints = 2;
         Point[] points = new Point[] {new Point(1.0, 0.0), new Point(0.0, 0.0)};
-        Parameters parameters = new Parameters();
-        CMV cmv = new CMV(numpoints, points, parameters);
+        CMV cmv = new CMV(numpoints, points, randomParameters);
 
         assertTrue(cmv.lic5());
     }
@@ -35,8 +35,7 @@ public class CMVTest {
         // lic5 should be false if two concecutive points have the same x value.
         int numpoints = 2;
         Point[] points = new Point[] {new Point(0.0, 0.0), new Point(0.0, 0.0)};
-        Parameters parameters = new Parameters();
-        CMV cmv = new CMV(numpoints, points, parameters);
+        CMV cmv = new CMV(numpoints, points, randomParameters);
 
         assertFalse(cmv.lic5());
     }
@@ -45,8 +44,7 @@ public class CMVTest {
     void lic5FewerThanTwoPoints() {
         int numpoints = 1;
         Point[] points = new Point[] {new Point(0.0, 0.0)};
-        Parameters parameters = new Parameters();
-        CMV cmv = new CMV(numpoints, points, parameters);
+        CMV cmv = new CMV(numpoints, points, randomParameters);
 
         assertFalse(cmv.lic5());
     }
