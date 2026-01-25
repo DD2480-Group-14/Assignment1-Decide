f7e71fbe5e96a1ef80a653cd1eda48bba6d5544f
melkerolle <melkertrane@outlook.com>
2026-01-25 01:11:14 +0100

    refactor - removed numpoints from CMV constructor

--- src/test/java/com/decide/app/calculators/CMVTest.java
+++ src/test/java/com/decide/app/calculators/CMVTest.java
@@ -22,10 +22,9 @@ public class CMVTest {
 
     @Test
     void lic5Positive() {
-        int numpoints = 2;
         Point[] points = new Point[] {new Point(1.0, 0.0), new Point(0.0, 0.0)};
         Parameters parameters = new Parameters();
-        CMV cmv = new CMV(numpoints, points, parameters);
+        CMV cmv = new CMV(points, parameters);
 
         assertTrue(cmv.lic5());
     }
@@ -33,20 +32,18 @@ public class CMVTest {
     @Test
     void lic5FalseBoundryCase() {
         // lic5 should be false if two concecutive points have the same x value.
-        int numpoints = 2;
         Point[] points = new Point[] {new Point(0.0, 0.0), new Point(0.0, 0.0)};
         Parameters parameters = new Parameters();
-        CMV cmv = new CMV(numpoints, points, parameters);
+        CMV cmv = new CMV(points, parameters);
 
         assertFalse(cmv.lic5());
     }
 
     @Test
     void lic5FewerThanTwoPoints() {
-        int numpoints = 1;
         Point[] points = new Point[] {new Point(0.0, 0.0)};
         Parameters parameters = new Parameters();
-        CMV cmv = new CMV(numpoints, points, parameters);
+        CMV cmv = new CMV(points, parameters);
 
         assertFalse(cmv.lic5());
     }
