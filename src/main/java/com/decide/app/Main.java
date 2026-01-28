6e0736b42729f6a5f8988a592ab9d37980d1afc7
Shrek <edwin03@live.se>
2026-01-28 15:03:33 +0100

    feat: add file containing decide method

---
+++ src/main/java/com/decide/app/Main.java
@@ -0,0 +1,35 @@
+package com.decide.app;
+
+import com.decide.app.calculators.*;
+import com.decide.app.model.*;
+
+public class Main {
+    private int numpoints;
+    private Point[] points;
+    private Parameters parameters;
+    private Connectors[][] lcm;
+    private boolean[] puv;
+
+    public Main(int numpoints, Point[] points, Parameters parameters, Connectors[][] lcm, boolean[] puv) {
+        this.numpoints = numpoints;
+        this.points = points;
+        this.parameters = parameters;
+        this.lcm = lcm;
+        this.puv = puv;
+    }
+
+    public boolean decide() {
+        boolean[] cmv = new CMV(points, parameters).calculateCMV();
+        boolean[][] pum = new PUM(cmv, lcm).calculatePUM();
+        boolean[] fuv = new FUV(puv, pum).calculateFUV();
+
+        for(int i = 0; i < 15; ++i) {
+            if(!fuv[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
